13890a602945aa47d6ea4eee5f10e7b32c034da0
Caio <caiosantana.lkd@gmail.com>
2026-02-16 12:15:48 -0300

    participantes e atividades

--- src/main/java/com/Desafio/Modelo/de/dominio/e/ORM/entities/Participante.java
+++ src/main/java/com/Desafio/Modelo/de/dominio/e/ORM/entities/Participante.java
@@ -11,10 +11,56 @@ public class Participante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String name;
+    private String nome;
 
     @Column(unique = true)
     private String email;
 
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "atividades_id")
+    private Atividade atividades;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Atividade getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(Atividade atividades) {
+        this.atividades = atividades;
+    }
+
+    public Participante(Integer id, String nome, String email, Atividade atividades) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.atividades = atividades;
+    }
+
+    public Participante() {
+    }
 }
